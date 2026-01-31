0930d17a710eb7df3711178f2caab11c4f440498
Tim Conze <tim.conze@gmail.com>
2026-01-31 18:30:56 +0000

    fixed update not working, but with own execution

--- server/updater/update.go
+++ server/updater/update.go
@@ -1,19 +1,27 @@
 package updater
 
 import (
+	"log/slog"
 	"os/exec"
-
-	"github.com/marcopiovanello/yt-dlp-web-ui/v3/server/config"
+	// "github.com/marcopiovanello/yt-dlp-web-ui/v3/server/config"
 )
 
 // Update using the builtin function of yt-dlp
 func UpdateExecutable() error {
-	cmd := exec.Command(config.Instance().DownloaderPath, "-U")
+	// cmd := exec.Command(config.Instance().DownloaderPath, "-U")
+	cmd := exec.Command("pip", "install", "-U", "yt-dlp", "--break-system-packages")
 
-	err := cmd.Start()
-	if err != nil {
-		return err
-	}
+	// err := cmd.Start()
 
-	return cmd.Wait()
+	// if err != nil {
+	// 	return err
+	// }
+
+	// return cmd.Wait()
+
+	out, err := cmd.CombinedOutput()
+	slog.Warn(string(out))
+
+	return err
+
 }
