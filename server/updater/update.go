5b2a8f93123ce98456b10021f5d1e5485029cbd4
Tim Conze <tim.conze@gmail.com>
2026-01-31 18:34:14 +0000

    Code cleanup removed old unused code

--- server/updater/update.go
+++ server/updater/update.go
@@ -1,27 +1,19 @@
 package updater
 
 import (
-	"log/slog"
 	"os/exec"
-	// "github.com/marcopiovanello/yt-dlp-web-ui/v3/server/config"
 )
 
 // Update using the builtin function of yt-dlp
 func UpdateExecutable() error {
-	// cmd := exec.Command(config.Instance().DownloaderPath, "-U")
 	cmd := exec.Command("pip", "install", "-U", "yt-dlp", "--break-system-packages")
 
-	// err := cmd.Start()
+	err := cmd.Start()
 
-	// if err != nil {
-	// 	return err
-	// }
+	if err != nil {
+		return err
+	}
 
-	// return cmd.Wait()
-
-	out, err := cmd.CombinedOutput()
-	slog.Warn(string(out))
-
-	return err
+	return cmd.Wait()
 
 }
